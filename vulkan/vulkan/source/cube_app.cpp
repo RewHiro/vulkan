37da2b8dde337e25fee33887ae6da31e3de189fe
DESKTOP-13M5OCA\hiro1 <hiro1215.turquoise@gmail.com>
2021-06-22 23:39:42 +0900

    プリミティブトポロジー設定

--- vulkan/vulkan/source/cube_app.cpp
+++ vulkan/vulkan/source/cube_app.cpp
@@ -74,6 +74,10 @@ namespace app
 		pipelineViewportStateCreateInfo.pViewports = &viewport;
 		pipelineViewportStateCreateInfo.scissorCount = 1;
 		pipelineViewportStateCreateInfo.pScissors = &scissor;
+
+		VkPipelineInputAssemblyStateCreateInfo pipelineInputAssemblyStateCreateInfo{};
+		pipelineInputAssemblyStateCreateInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_INPUT_ASSEMBLY_STATE_CREATE_INFO;
+		pipelineInputAssemblyStateCreateInfo.topology = VK_PRIMITIVE_TOPOLOGY_TRIANGLE_LIST;
 	}
 
 	void CubeApp::makeCubeGeometry()
