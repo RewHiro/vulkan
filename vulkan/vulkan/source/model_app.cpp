78372030fbf45739a5dc2c20272772ed9b881716
DESKTOP-13M5OCA\hiro1 <hiro1215.turquoise@gmail.com>
2021-06-26 14:04:16 +0900

    モデルのファイル読み込み

--- vulkan/vulkan/source/model_app.cpp
+++ vulkan/vulkan/source/model_app.cpp
@@ -1,8 +1,22 @@
 #include "model_app.hpp"
 
+#include "stream_reader.hpp"
+
 namespace app
 {
 	void ModelApp::prepare()
 	{
+		auto modelFilePath = std::filesystem::path("alicia-solid.vrm");
+		if (modelFilePath.is_relative())
+		{
+			auto current = std::filesystem::current_path();
+			current /= modelFilePath;
+			current.swap(modelFilePath);
+		}
+
+		auto reader = std::make_unique<StreamReader>(modelFilePath.parent_path());
+		auto glbStream = reader->GetInputStream(modelFilePath.filename().u8string());
+		auto glbResourceReader = std::make_shared<Microsoft::glTF::GLBResourceReader>(std::move(reader), std::move(glbStream));
+		auto document = Microsoft::glTF::Deserialize(glbResourceReader->GetJson());
 	}
 }
